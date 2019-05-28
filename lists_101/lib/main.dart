90907791e06a2a42ee904999d836ddab103a3499
JIthinlal <jithtitan007@gmail.com>
2019-05-28 11:13:53 +0530

    lists complete

--- lists_101/lib/main.dart
+++ lists_101/lib/main.dart
@@ -1,111 +1,193 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// 1. Basic list #BasicList
+// 2. Horizontal list #HorizontalList
+// 3. Grid list #GridList
+// 4. Alternate list generators #AlernateList
+// 5. Flexible appbar list #FlexibleAppbarList
+void main() => runApp(FlexibleAppbarList());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BasicList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Basic List',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Basic List'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Photo'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            )
+          ],
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HorizontalList extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Horizontal list',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Horizontal List'),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 200,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 160.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class GridList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Horizontal list',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Horizontal List'),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
   }
+}
+
+class AlternateList extends StatelessWidget {
+  final List<ListItem> lists = List<ListItem>.generate(1000, (i) {
+    return i % 6 == 0
+        ? HeadingItem('Heading $i')
+        : MessageItem('Sender $i', 'Message ');
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Horizontal list',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Horizontal List'),
+        ),
+        body: ListView.builder(
+            itemCount: lists.length,
+            itemBuilder: (context2, index) {
+              final item = lists[index];
+              if (item is HeadingItem) {
+                return ListTile(
+                  title: Text(
+                    item.heading,
+                    style: Theme.of(context2).textTheme.headline,
+                  ),
+                );
+              } else if (item is MessageItem) {
+                return ListTile(
+                  title: Text(item.sender),
+                  subtitle: Text(item.body),
+                );
+              }
+            }),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+abstract class ListItem {}
+
+class HeadingItem implements ListItem {
+  final String heading;
+  HeadingItem(this.heading);
+}
+
+class MessageItem implements ListItem {
+  final String body;
+  final String sender;
+
+  MessageItem(this.sender, this.body);
+}
+
+class FlexibleAppbarList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flexible Appbar',
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              title: Text('Flexible List'),
+              floating: true,
+              flexibleSpace: Container(
+                color: Colors.red,
+              ),
+              expandedHeight: 200,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context2, index) {
+                  return ListTile(
+                    title: Text('Item $index'),
+                  );
+                },
+                childCount: 1000,
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
